9918b6e46d19842db6eceae671731c43b97ef94c
NareshVNA <nareshawsacc@gmail.com>
2024-12-12 18:04:51 +0530

    Creating  main source file for ec2 module

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2inst" {
+ami = "ami-0614680123427b75e"
+instance_type = "t2.micro"
+
+tags = {
+    Name = "Ec2_Instance"
+}
