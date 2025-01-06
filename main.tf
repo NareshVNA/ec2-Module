8db8d710a7ff4e1f57c046c211625e76bb3b967d
NareshV <nareshawsacc@gmail.com>
2025-01-06 17:32:15 +0530

    updated to build t3.micro machine for argoCD

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "ec2inst" {
-ami = "ami-0614680123427b75e"
-instance_type = "t2.micro"
+ami = "ami-0fd05997b4dff7aac"
+instance_type = "t3.medium"
 
 tags = {
-    Name = "Ec2_Instance"
+    Name = "miniKube-VM"
 }
 }
