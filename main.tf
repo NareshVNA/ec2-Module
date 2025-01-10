06318a584b59bb47e66b97840fa02691a0306ac4
NareshV <nareshawsacc@gmail.com>
2025-01-10 14:23:10 +0530

    brought in right configurations

--- main.tf
+++ main.tf
@@ -2,24 +2,42 @@ provider "aws" {
 region = "ap-south-1"
 }
 
-resource "aws_instance" "ec2inst" {
+resource "aws_instance" "miniKube_VM" {
 ami = "ami-0fd05997b4dff7aac"
 instance_type = "t3.medium"
+key_name = "miniKube"
+vpc_security_group_ids = [aws_security_group.miniKubeSG.id]
 
 tags = {
     Name = "miniKube-VM"
 }
 
 provisioner "file" {
-    source      = "DesiredConfig.sh"
-    destination = "/tmp/DesiredConfig.sh"
-  }
-
-provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/DesiredConfig.sh",
-      "/tmp/DesiredConfig.sh",
-    ]
-  }
+  source = "./DesiredConfig.sh"
+  destination = "/tmp/DesiredConfig.sh"
+}
+user_data = file("DesiredConfig.sh")
 }
 
+resource "aws_security_group" "miniKubeSG" {
+  name        = "miniKubeSG"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "MiniKubeSG"
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "miniKube_ingress" {
+  security_group_id = aws_security_group.miniKubeSG.id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+
+}
+
+resource "aws_vpc_security_group_egress_rule" "miniKube_egress" {
+   security_group_id = aws_security_group.miniKubeSG.id
+   cidr_ipv4   = "0.0.0.0/0"
+   ip_protocol = "-1"
+}
